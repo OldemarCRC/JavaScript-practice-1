54fb9097d854135e18aedf436fc8b8fa63099b09
Oldemar Chaves <oldemar.chaves@gmail.com>
2023-04-22 23:15:15 +0300

    Changes in CSS to make in responsive

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ includes HTML, JavaScript and CSS files.
 I am doing this change just to see how this works.
 This is another change to use git pull from my local repository.
 This line is to change my local repository and push the changes into my GitHub repository, using git push.
+
+CSS Responsive Styles
+This repository contains changes made to the CSS file to make it responsive on various screen sizes. The following styles were applied for different screen sizes and orientations:
+
+General Styles
+Screen size greater than 1440px
+Screen size between 1025px and 1440px
+Screen size between 768px and 1024px in portrait orientation
+Screen size between 768px and 1024px in landscape orientation
+Screen size less than 414px in portrait orientation
+Screen height less than 414px in landscape orientation
+Screen height less than 414px and width greater than 896px in landscape orientation
+These styles will ensure that the website looks good and functions well across various devices and screen sizes.
--- script.js
+++ script.js
@@ -1,44 +1,46 @@
+'use strict'
 const date_time = new Date();
-        document.getElementById("date-time").innerHTML = date_time;
-        let year = date_time.getFullYear(); //getFullYear() method
-        document.getElementById("year").innerHTML = year;
-        const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-        let name = month[date_time.getMonth()]; // getMonth() method
-        document.getElementById("m").innerHTML = name;
-        let date = date_time.getDate(); //getDate() method
-        document.getElementById("date").innerHTML = date;
-        let hour = date_time.getHours(); //getHours() method
-        document.getElementById("h").innerHTML = hour;
-        let minutes = date_time.getMinutes(); //getMinutes() method
-        document.getElementById("min").innerHTML = minutes;
-        
-        function checkAge() {
-          var age = document.getElementById("age").value;
-          if (age >= 18) {
-            document.getElementById("message").innerHTML = "You are an adult";
-          } else {
-            document.getElementById("message").innerHTML = "You are underage";
-          }
-        }
-        function checkMacdonalds() {
-          var age = document.getElementById("age").value;
-          if (age >= 16 && age <=25) {
-              document.getElementById("macmessage").innerHTML = "You can work with us";
-          } else {
-              document.getElementById("macmessage").innerHTML = "Sorry! You can't work with us";
-          }
-        }
-        function checkLetter() {
-          var letter = document.getElementById("letter").value;;
-          var msg = "";
-          switch (letter) {
-            case "n":
-              msg = "Oldemar";
-              document.getElementById("my_name").innerHTML = "My name is " + msg;
-              break;
-            default:
-              msg = "Variable n was not assigned";
-              document.getElementById("my_name").innerHTML = msg;
-              break;
-            }
-        }
+document.getElementById("date-time").innerHTML = date_time;
+let year = date_time.getFullYear(); //getFullYear() method
+document.getElementById("year").innerHTML = year;
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let name = month[date_time.getMonth()]; // getMonth() method
+document.getElementById("m").innerHTML = name;
+let date = date_time.getDate(); //getDate() method
+document.getElementById("date").innerHTML = date;
+let hour = date_time.getHours(); //getHours() method
+document.getElementById("h").innerHTML = hour;
+let minutes = date_time.getMinutes(); //getMinutes() method
+document.getElementById("min").innerHTML = minutes;
+
+function checkAge() {
+  var age = document.getElementById("age").value;
+  if (age < 18 && age >= 12) {
+    document.getElementById("message").innerHTML = "You are a teenager.";
+
+  } else if (age >= 6 && age < 12) {
+    document.getElementById("message").innerHTML = "You are an alementary school student.";
+
+  } else if (age >= 0 && age < 6) {
+    document.getElementById("message").innerHTML = "You are a baby.";
+
+  } else if (age >= 18) {
+    document.getElementById("message").innerHTML = "You are an adult.";
+  }
+
+}
+
+function checkLetter() {
+  var letter = document.getElementById("letter").value;;
+  var msg = "";
+  switch (letter) {
+    case "n":
+      msg = "Oldemar";
+      document.getElementById("my_name").innerHTML = "My name is " + msg;
+      break;
+    default:
+      msg = "Variable n was not assigned";
+      document.getElementById("my_name").innerHTML = msg;
+      break;
+  }
+}
